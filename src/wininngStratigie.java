134cca9c8c981a25f8f52fe0a3f78d9fd23a0dfa
QmrMalla <qmr.malla.it@gmail.com>
2023-12-07 15:04:53 +0100

    create wininng Stratigie method

--- src/wininngStratigie.java
+++ src/wininngStratigie.java
@@ -1,4 +1,14 @@
+import java.util.Random;
 
 public class wininngStratigie {
+	private static final int MAX_MOVE = 3;
+	public static int calculateComputerMove(int matches) {
+        Random random = new Random();
 
+        if (matches % (MAX_MOVE + 1) == 0) {
+            return random.nextInt(MAX_MOVE) + 1;
+        } else {
+            return matches % (MAX_MOVE + 1);
+        }
+    }
 }
